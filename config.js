9d981be363dbd7fea5a2837c125643c46b7dcc26
SilenceFox <47704491+SilenceFox@users.noreply.github.com>
2021-10-12 13:17:48 -0300

    Update config.js

--- config.js
+++ config.js
@@ -11,26 +11,26 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
+  name: 'Doot',
   imageBackground: false,
   openInNewTab: true,
   twelveHourFormat: false,
 
   // Greetings
-  greetingMorning: 'Good morning!',
-  greetingAfternoon: 'Good afternoon,',
-  greetingEvening: 'Good evening,',
-  greetingNight: 'Go to Sleep!',
+  greetingMorning: 'Bom dia Guri!',
+  greetingAfternoon: 'Boa tarde guri!,',
+  greetingEvening: 'Boa noite guri!,',
+  greetingNight: 'Vá mimir guri!!',
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456',
-  weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
+  weatherKey: '2032b8ae2be3384073c76152ca8ec8bd',
+  weatherIcons: 'Nord', // 'Onedark', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
-  language: 'en', // More languages in https://openweathermap.org/current#multi
+  language: 'pt_br', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '37.775',
-  defaultLongitude: '-122.419',
+  defaultLatitude: '29.996320',
+  defaultLongitude: '-51.058500',
 
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
   // │  ├─┤├┬┘ ││└─┐
